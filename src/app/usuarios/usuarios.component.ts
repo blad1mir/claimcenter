d4373c6ee84a2090316dec1dc24413ef2e710fb3
Bladimir Aponte <bladimirapon@gmail.com>
2024-08-26 01:34:00 -0400

    get contact details

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -285,7 +285,7 @@ public usuario: Contacto = {
       this.changeVisibility(index)
       console.log(id)
    // this.service.getData('user_profiles/?'+queryString).subscribe(
-      this.service.getContacts('user_profiles/'+id).subscribe(
+      this.service.getContacts('user_profiles/'+id+'/').subscribe(
         (response) => {
           console.log(response)
   
