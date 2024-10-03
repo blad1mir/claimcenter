79e077ce69e874e62ac4264f5887c98ad667ba4d
Bladimir Aponte <bladimirapon@gmail.com>
2024-10-03 05:05:38 -0400

    comments by modify

--- src/app/expedientes/expedientes.component.ts
+++ src/app/expedientes/expedientes.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expedientes.component.scss']
 })
 export class ExpedientesComponent implements OnInit {
+  public searchTerm: string = '';
+  public listado_expedientes: any[] = [];
+filtered_expedientes: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
