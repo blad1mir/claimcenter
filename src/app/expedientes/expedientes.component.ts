8ba4f14a4300627d8a0e7dbda396fdf1b460b25b
Bladimir Aponte <bladimirapon@gmail.com>
2024-11-27 03:35:37 -0400

    list by filter

--- src/app/expedientes/expedientes.component.ts
+++ src/app/expedientes/expedientes.component.ts
@@ -10,6 +10,7 @@ import { FileDetails } from '../core/interfaces/company';
 })
 export class ExpedientesComponent implements OnInit {
   public searchTerm: string = '';
+  public searchTerms: string = '';
   public nextPag: string = '';
   public previusPag: string = '';
   public canElementos: string = '';
@@ -22,8 +23,19 @@ export class ExpedientesComponent implements OnInit {
     'Datos del asegurado. Situación del riesgo',
     'Datos de la póliza',
     'Datos del siniestro',
-    'Facturacion'
-  ]
+    'Facturacion',
+  ];
+  nombresExpediente = [
+    'Gestión de siniestro sector asegurador',
+    'Gestión de siniestro internacional',
+    'Gestión siniestro médico sector asegurador',
+    'Particulares',
+    'Procedimiento civil',
+    'Procedimiento penal',
+    'Procedimiento administrativo',
+    'Asunto pericial técnico especializado',
+    'Tasación',
+  ];
   //dfd
 
   public listado_empresas: any[] = [];
@@ -34,17 +46,18 @@ export class ExpedientesComponent implements OnInit {
   formGroup!: FormGroup;
   activeStep = 0;
   allInputs = [
-    { id: 'file_id', label: 'File ID' }, 
-    { id: 'code', label: 'Code' }, 
-    { id: 'start_date', label: 'Start Date' }, 
-    { id: 'end_date', label: 'End Date' }, 
-    { id: 'assignment', label: 'Assignment' }, 
-    { id: 'description', label: 'Description' }, 
-    { id: 'type', label: 'Type' }, 
-    { id: 'created_by', label: 'Created By' }, 
-    { id: 'status', label: 'Status' }, 
-    { id: 'billing_issuer', label: 'Billing Issuer' }, 
-    { id: 'attributes', label: 'Attributes' } ];
+    { id: 'file_id', label: 'File ID' },
+    { id: 'code', label: 'Code' },
+    { id: 'start_date', label: 'Start Date' },
+    { id: 'end_date', label: 'End Date' },
+    { id: 'assignment', label: 'Assignment' },
+    { id: 'description', label: 'Description' },
+    { id: 'type', label: 'Type' },
+    { id: 'created_by', label: 'Created By' },
+    { id: 'status', label: 'Status' },
+    { id: 'billing_issuer', label: 'Billing Issuer' },
+    { id: 'attributes', label: 'Attributes' },
+  ];
 
   initialFileDetails: FileDetails = {
     file_id: 0,
@@ -68,22 +81,25 @@ export class ExpedientesComponent implements OnInit {
   };
   activeTab = 0;
   openTab = 1;
-toggleTabs($tabNumber: number){
-  this.openTab = $tabNumber;
-}
+  toggleTabs($tabNumber: number) {
+    this.openTab = $tabNumber;
+  }
 
   constructor(public service: DataService, private fb: FormBuilder) {
-this.getListCompany();
-
+    this.getListCompany();
   }
 
   ngOnInit(): void {
-     this.getExpedientes()
-    const group: any = {}; 
-    this.allInputs.forEach(input => { group[input.id] = [this.initialFileDetails[input.id as keyof FileDetails], Validators.required]; }); 
-    this.formGroup = this.fb.group({})
+    this.getExpedientes();
+    const group: any = {};
+    this.allInputs.forEach((input) => {
+      group[input.id] = [
+        this.initialFileDetails[input.id as keyof FileDetails],
+        Validators.required,
+      ];
+    });
+    this.formGroup = this.fb.group({});
   }
-
 
   onSubmit() {
     if (this.formGroup.valid) {
@@ -115,210 +131,202 @@ this.getListCompany();
 
   onSearch(): void {
     if (this.searchTerm) {
-      this.service.getData(`incident_files/?search=${this.searchTerm}`).subscribe(
-        (response) => {
-          console.log(response)
-        this.listado_expedientes = response.results;
-        this.filtered_expedientes = response.results;
-        this.nextPag = response.next;
-        this.previusPag = response.previous;
-        if(response.count <= 10){
-          if(response.count == 0){
-            this.canElementos = '0';
-          }else{
-            this.canElementos = '1';
+      this.service
+        .getData(`incident_files/?search=${this.searchTerm}`)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.listado_expedientes = response.results;
+            this.filtered_expedientes = response.results;
+            this.nextPag = response.next;
+            this.previusPag = response.previous;
+            if (response.count <= 10) {
+              if (response.count == 0) {
+                this.canElementos = '0';
+              } else {
+                this.canElementos = '1';
+              }
+            } else {
+              this.canElementos = (response.count / 10).toFixed(0);
+            }
+            console.log(this.listado_expedientes);
+          },
+          (error) => {
+            console.error('Error al traer listado de empresas:', error);
+            // Manejar el error en el login
           }
-          
-        }else{
-          this.canElementos = ((response.count)/10).toFixed(0);
-        }
-        console.log( this.listado_expedientes)
-         
-        },
-        error => {
-          console.error('Error al traer listado de empresas:', error);
-          // Manejar el error en el login
-        });
-      } 
+        );
+    }
     // console.log(this.searchTerm)
     // this.filtered_empresas = this.listado_empresas.filter((empresa: any) => {
     //   // Verificar que los campos sean strings válidos antes de llamar a toLowerCase()
     //   const nombre = empresa.nombre ? empresa.nombre : '';
     //   const email = empresa.email ? empresa.email : '';
     //   const telefono = empresa.telefono ? empresa.telefono : '';
-    //   return nombre.includes(this.searchTerm) || 
-    //          email.includes(this.searchTerm) || 
+    //   return nombre.includes(this.searchTerm) ||
+    //          email.includes(this.searchTerm) ||
     //          telefono.includes(this.searchTerm);
     // });
   }
 
-  nextPage(){
-
-
+  nextPage() {
     if (this.nextPag != null) {
-      
-    const url = this.nextPag;
-    const queryString = url.split('?')[1];
-    console.log(queryString);
-          this.service.getData('incident_files/?'+queryString).subscribe(
-      (response) => {
-        console.log(response)
-      this.listado_expedientes = response.results;
-      this.filtered_expedientes = response.results;
-      this.nextPag = response.next;
-      this.previusPag = response.previous;
-      if(response.count <= 10){
-        if(response.count == 0){
-          this.canElementos = '0';
-        }else{
-          this.canElementos = '1';
+      const url = this.nextPag;
+      const queryString = url.split('?')[1];
+      console.log(queryString);
+      this.service.getData('incident_files/?' + queryString).subscribe(
+        (response) => {
+          console.log(response);
+          this.listado_expedientes = response.results;
+          this.filtered_expedientes = response.results;
+          this.nextPag = response.next;
+          this.previusPag = response.previous;
+          if (response.count <= 10) {
+            if (response.count == 0) {
+              this.canElementos = '0';
+            } else {
+              this.canElementos = '1';
+            }
+          } else {
+            this.canElementos = (response.count / 10).toFixed(0);
+          }
+          console.log(this.listado_expedientes);
+        },
+        (error) => {
+          console.error('Error al traer listado de empresas:', error);
+          // Manejar el error en el login
         }
-        
-      }else{
-        this.canElementos = ((response.count)/10).toFixed(0);
-      }
-      console.log( this.listado_expedientes)
-       
-      },
-      error => {
-        console.error('Error al traer listado de empresas:', error);
-        // Manejar el error en el login
-      });
+      );
     }
-
-
   }
-  previusPage(){
-   
+  previusPage() {
     if (this.previusPag != null) {
       const url = this.previusPag;
       const queryString = url.split('?')[1];
       console.log(queryString);
-          this.service.getData('incident_files/?'+queryString).subscribe(
-      (response) => {
-        console.log(response)
-      this.listado_expedientes = response.results;
-      this.filtered_expedientes = response.results;
-      this.nextPag = response.next;
-      this.previusPag = response.previous;
-      if(response.count <= 10){
-        if(response.count == 0){
-          this.canElementos = '0';
-        }else{
-          this.canElementos = '1';
+      this.service.getData('incident_files/?' + queryString).subscribe(
+        (response) => {
+          console.log(response);
+          this.listado_expedientes = response.results;
+          this.filtered_expedientes = response.results;
+          this.nextPag = response.next;
+          this.previusPag = response.previous;
+          if (response.count <= 10) {
+            if (response.count == 0) {
+              this.canElementos = '0';
+            } else {
+              this.canElementos = '1';
+            }
+          } else {
+            this.canElementos = (response.count / 10).toFixed(0);
+          }
+          console.log(this.listado_expedientes);
+        },
+        (error) => {
+          console.error('Error al traer listado de empresas:', error);
+          // Manejar el error en el login
         }
-        
-      }else{
-        this.canElementos = ((response.count)/10).toFixed(0);
-      }
-      console.log( this.listado_expedientes)
-       
-      },
-      error => {
-        console.error('Error al traer listado de empresas:', error);
-        // Manejar el error en el login
-      });
+      );
     }
-
-
-
   }
 
   changeVisibility(value: number): void {
     this.option = value;
-   // this.service.setOptionChange(this.option)
+    // this.service.setOptionChange(this.option)
     // if (this.option != 1 && this.option != 3 ) {
-      
+
     //   this.service.setChange(true)
     // }else{
-     
+
     //   this.service.setChange(false)
     // }
-    console.log('El valor actual de option es: '+this.option)
-    
+    console.log('El valor actual de option es: ' + this.option);
+
     // if (this.option == 3 || this.option ==1) {
     //   this.service.setChange(false)
     // }
   }
 
   getListCompany() {
-
     this.service.getData('enterprises/').subscribe(
       (response) => {
-        console.log(response)
-      this.listado_empresas = response.results;
-      console.log( this.listado_empresas)
-       
+        console.log(response);
+        this.listado_empresas = response.results;
+        console.log(this.listado_empresas);
       },
-      error => {
+      (error) => {
         console.error('Error al traer listado de empresas:', error);
         // Manejar el error en el login
-      });
-
-
+      }
+    );
   }
 
   getExpedientParticular(id: string) {
-
-    this.service.getEnterprise('incident_files/'+id+'/').subscribe(
+    this.service.getEnterprise('incident_files/' + id + '/').subscribe(
       (response) => {
-        console.log(response)
-      this.initialFileDetails= response.results;
-       
+        console.log(response);
+        this.initialFileDetails = response.results;
       },
-      error => {
+      (error) => {
         console.error('Error al traer listado de empresas:', error);
         // Manejar el error en el login
-      });
-
-
+      }
+    );
   }
 
-  createExpedient(){
+  createExpedient() {
     let expediente = {
-      
-        assignment: this.initialFileDetails.assignment,
-        description: this.initialFileDetails.description,
-        type: this.initialFileDetails.type,
-        status: "created"
-    
-    }
+      assignment: this.initialFileDetails.assignment,
+      description: this.initialFileDetails.description,
+      type: this.initialFileDetails.type,
+      status: 'created',
+    };
 
-    console.log(expediente)
+    console.log(expediente);
     if (!expediente.assignment || !expediente.description || !expediente.type) {
-      console.log("Por favor, complete todos los campos", "", {
-        duration: 2000
-      })
-      
-    }else{
-      this.service.postData('incident_files/',expediente).subscribe(
+      console.log('Por favor, complete todos los campos', '', {
+        duration: 2000,
+      });
+    } else {
+      this.service.postData('incident_files/', expediente).subscribe(
         (response) => {
-  
-          if(response)
-          console.log('Registro creado correctamente', response);
-          console.log("Registro creado correctamente", "", {
-            duration: 2000
-          })
-         this.option = 0;
+          if (response) console.log('Registro creado correctamente', response);
+          console.log('Registro creado correctamente', '', {
+            duration: 2000,
+          });
+          this.option = 0;
         },
-        error => {
+        (error) => {
           console.error('El expediente no se pudo crear:', error);
           // Manejar el error en el login
-          console.log("El expediente no se pudo crear", "", {
-            duration: 2000
-          })
-        });
+          console.log('El expediente no se pudo crear', '', {
+            duration: 2000,
+          });
+        }
+      );
     }
-
   }
 
-  openExpedient(code: string, id: string){
-    this.getExpedientParticular(id)
+  openExpedient(code: string, id: string) {
+    this.getExpedientParticular(id);
     this.codeExpedient = code;
     this.option = 2;
   }
 
   setActiveTab(index: number) {
     this.activeTab = index;
+  }
+
+  filteredNombresExpediente = [...this.nombresExpediente];
+  filterOptions() {
+    this.filteredNombresExpediente = this.nombresExpediente.filter((nombre) =>
+      nombre.toLowerCase().includes(this.searchTerms.toLowerCase())
+    );
+  }
+
+  selectOption(nombre: string) {
+    this.initialFileDetails.type = nombre;
+    this.searchTerms = nombre;
+    this.filteredNombresExpediente = [];
   }
 }
